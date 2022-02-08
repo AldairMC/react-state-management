659496628d23a72be17ab080664c83d6195f516b
AldairMC <aldairmc2023@gmail.com>
2022-02-07 23:06:47 -0500

    :sparkles: Component User ready, waiting for test

--- src/components/users/User.jsx
+++ src/components/users/User.jsx
@@ -3,8 +3,9 @@ import React from "react";
 const User = ({ user }) => {
     return (
         <div className="_user_container_">
-            <img src={user.avatar} alt="Avatar" width={150} />
-            From User {user.email}
+            <img src={user.avatar} alt="Avatar" width={100} />
+            <span>{user.first_name} {user.last_name}</span>
+            <span>{user.email}</span>
         </div>
     )
 }
